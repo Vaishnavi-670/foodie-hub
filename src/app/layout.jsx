f8d4f418378db64960720c4383c7dd680a3cf678
Vaishnavi-670 <chauhcomanvaishnavi684@gmail.com>
2024-07-31 23:09:04 +0530

    chore: Add Navbar component to layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,9 @@
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { useState } from "react";
+import Navbar from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
-const [login, setlogin] = useState(true)
 
 export const metadata = {
   title: "Create Next App",
@@ -15,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      
+      <Navbar/>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,28 +5,8 @@ import React from 'react'
 const page = () => {
   return (
     
-      <div className="hero">
-        <nav className="flex justify-between items-center h-20 w-full">
-          <div className="nav-image">
-          <img style={{ width: 150 }} src='https://s.tmimgcdn.com/scr/1200x627/242400/food-delivery-custom-design-logo-template_242462-original.png'></img>
-          </div>
-          <div className="nav-link">
-            <ul className='flex list-none'>
-              <li className='mx-2 my-2 px-2'>
-                <Link href="/login" className="text-black text-lg font-medium no-underline">Login</Link>
-              </li>
-              <li className='mx-2 my-2 px-2'>
-                <Link href="#" className="text-black text-lg font-medium no-underline">About</Link>
-              </li>
-              <li className='mx-2 my-2 px-2'>
-                <Link href="#" className="text-black text-lg font-medium no-underline">Service</Link>
-              </li>
-              <li className='mx-2 my-2 px-2'>
-                <Link href="#" className="text-black text-lg font-medium no-underline">Contact</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+      <div className="">
+        
       </div>
     
   )
