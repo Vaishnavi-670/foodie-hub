632b004cdc3cba47dc227aa3d900ad04101f7f11
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-08-11 14:33:53 +0530

    .

---
+++ backend/connection.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const url = "mongodb+srv://vaishnavi357:vaishnavi123@cluster0.k4yrt8l.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(url)
