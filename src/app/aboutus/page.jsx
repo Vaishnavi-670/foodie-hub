b9c02e60c0adfa2c880cd4589801289deb79938d
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-10-16 22:59:00 +0530

    v

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -5,7 +5,6 @@ const AboutUsPage = () => {
         <div className="min-h-screen bg-orange-50 text-gray-800">
             <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8">
 
-                {/* Introduction Section */}
                 <div className="text-center mb-12">
                     <h1 className="text-4xl font-bold text-orange-900 mb-4">About Foodie Hub</h1>
                     <p className="text-lg max-w-2xl mx-auto">
