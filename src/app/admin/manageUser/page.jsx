7d1764abc32eb5bc1f1d19a53627da50fa342b01
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-10-21 21:49:07 +0530

    l

--- src/app/admin/manageUser/page.jsx
+++ src/app/admin/manageUser/page.jsx
@@ -37,7 +37,6 @@ useEffect(() => {
   return (
     
       <>
-  {/* Table Section */}
   <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     
     <div className="flex flex-col">
