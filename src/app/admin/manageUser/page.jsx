c6c3e9f4a6854db4e1d011e4f8bb5143ec8ef87f
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-10-21 21:50:39 +0530

    n

--- src/app/admin/manageUser/page.jsx
+++ src/app/admin/manageUser/page.jsx
@@ -84,7 +84,6 @@ useEffect(() => {
                 </div>
               </div>
             </div>
-            {/* End Header */}
             {/* Table */}
             <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
               <thead className="bg-gray-50 dark:bg-neutral-800">
