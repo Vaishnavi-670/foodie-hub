ceb24a34785a0da7350fafca7a7a2e9ef7f32e1a
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-10-21 21:48:48 +0530

    k

--- src/app/admin/manageUser/page.jsx
+++ src/app/admin/manageUser/page.jsx
@@ -39,7 +39,7 @@ useEffect(() => {
       <>
   {/* Table Section */}
   <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-    {/* Card */}
+    
     <div className="flex flex-col">
       <div className="-m-1.5 overflow-x-auto">
         <div className="p-1.5 min-w-full inline-block align-middle">
