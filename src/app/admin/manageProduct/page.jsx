09c92eff10ae1fd3653a0d9949e8c0a8804aa2bd
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-10-22 22:55:10 +0530

    n

--- src/app/admin/manageProduct/page.jsx
+++ src/app/admin/manageProduct/page.jsx
@@ -38,7 +38,7 @@ const ManageProduct = () => {
   return (
     <>
    <>
-  {/* Table Section */}
+ 
   <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     {/* Card */}
     <div className="flex flex-col">
