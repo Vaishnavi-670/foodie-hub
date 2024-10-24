9abb6ae8143922439eda164604f93f4710a2c707
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-10-24 21:33:45 +0530

    l

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -65,7 +65,6 @@ const ServicePage = () => {
             </div>
           </div>
         </div>
-        {/* Service 4: Cooking Classes */}
         <div className="mb-20">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
             <div className="order-2 md:order-1">
